2d10d3493fcd2125b47f4c1ae07bbd75a7887b9f
narun <houynaron@gmail.com>
2026-08-30 20:44:16 +0700

    Add context.

---
+++ Context.jsx
@@ -0,0 +1,15 @@
+<Login user={user}/>
+    <Welcome user={user}/>
+        <User user={user}/>
+            <Profile user={user}/>
+
+
+------
+Context:
+Context.Provider value={user}
+
+<Login/>
+    <Welcome/>
+        <User/>
+            <Profile/>
+                {user} = useContext(Context)
